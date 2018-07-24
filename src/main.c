9fe7e95fb3b101299bf18a919973e86324839606
Böszörményi Zoltán <zboszormenyi@sicom.com>
2018-07-24 16:35:19 +0200

    Move constructing the DISPLAY number earlier
    and report it in systemctl status using the final variable.
    
    Signed-off-by: Böszörményi Zoltán <zboszormenyi@sicom.com>

--- src/main.c
+++ src/main.c
@@ -373,14 +373,15 @@ int main(int argc, char **argv)
 
 		if (pollval >= 0) {
 			char disp[16];
+			char xdisp[24];
 
 			if (read(pipe_fds[0], disp, sizeof(disp)) > 0) {
+				sprintf(xdisp, ":%s", disp);
+
 				if (pl_is_running) {
-					char xdisp[24];
 					Display *display;
 					int retries = 10;
 
-					sprintf(xdisp, ":%s", disp);
 
 					do {
 						display = XOpenDisplay(xdisp);
@@ -395,7 +396,7 @@ int main(int argc, char **argv)
 					} while (!display && retries);
 				}
 
-				sd_notifyf(0, "READY=1\nSTATUS=Xorg server started on DISPLAY=:%s\n", disp);
+				sd_notifyf(0, "READY=1\nSTATUS=Xorg server started on DISPLAY='%s'\n", xdisp);
 			} else
 				sd_notifyf(0, "READY=1\nSTATUS=Reading failed from -displayfd with errno %d\n", errno);
 		} else
